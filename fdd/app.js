35b6a4868e418f8c36b2a1a333ee598ea3c90c4b
Ellie1013git <kkaldl1013@naver.com>
2024-12-10 23:00:49 +0900

    Add files via upload
    print

--- fdd/app.js
+++ fdd/app.js
@@ -1,45 +1,45 @@
 const appUl=document.getElementById(
     "appUl")
-const appForm=document.forms.appForm;
+//const appForm=document.forms.appForm;
 //console.log(appUl);
 //console.log(appForm);
 const schForms=Array.from(document.getElementsByClassName("schForm"))
 console.log(schForms);
 const schArr=[];
-class sch {
+class Sch {
     constructor(form) {
-        this.
+        this. time=form.time.value;
+        this. part=form.part.value;
+        this. content=form.content.value;
     }
 }
-
+const printSchArr=function () {
+    let str=``;
+    for(let i of schArr){
+        str+=`<li>
+           <b> ${i.time}</b>
+           <span> ${i.part}</span>
+           <span> ${i.content}</span>
+           </li>
+        `
+    };
+    appUl.innerHTML=str;
+}
 const schSubmitHandler=function(e) {
     e.preventDefault();
     console.log(this);
+    let sch=new Sch(this);
+    console.log(sch);
+    let schs=schArr.filter((item)=>item.time===sch.time)
+    if(schs.length){
+        alert("이미 운동하셨습니다. 바꾸시겠습니까?")
+    }else{
+        schArr.push(sch);
+    };
+    console.log(schArr);
+    printSchArr();
 }
     
     schForms.forEach((form)=>{
         form.onsubmit=schSubmitHandler;   
     });
-
-
-
-// const printAppObj=function(){
-//     for(let key in appObj){
-//         let li=document.createElement("li");
-//         let b=document.createElement("b");
-//         let span=document.createElement("span");
-
-//         b.innerText=strTime(key);
-//         span.innerText=appObj[key];
-//         li.append(b);
-//         li.append(span);
-//         appUl.append(li);
-//     }
-// }
-// const strTime=function(time){
-//     let h=Math.floor(time/60);
-//     let m=time%60;
-//     return h+"시 "+m+"분";
-// }
-
-// printAppObj();
